6df1039c89009050eab81941e75fe95bf2c2b9b1
okcha4ever <minoukhiari4@gmail.com>
2023-11-04 20:28:16 +0100

    env.msg

--- src/env.mjs
+++ src/env.mjs
@@ -21,10 +21,6 @@ export const env = createEnv({
     NEXT_PUBLIC_CLERK_SECRET_KEY: z
       .string()
       .default("sk_test_hfnpirGagJiwlWoaVYr7lfQsDKlRjaW7sQclsaZqFz"),
-
-    // CLERK_SECRET_KEY: z
-    //   .string()
-    //   .default("sk_test_hfnpirGagJiwlWoaVYr7lfQsDKlRjaW7sQclsaZqFz"),
     NEXT_PUBLIC_CLERK_SIGN_IN_URL: z.string().default("/sign-in"),
     NEXT_PUBLIC_CLERK_SIGN_UP_URL: z.string().default("/sign-up"),
     NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: z.string().default("/"),
@@ -35,8 +31,7 @@ export const env = createEnv({
     NODE_ENV: process.env.NODE_ENV,
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
       process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-    NEXT_PUBLIC_CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
-    // CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    NEXT_PUBLIC_CLERK_SECRET_KEY: process.env.NEXT_PUBLIC_CLERK_SECRET_KEY,
     NEXT_PUBLIC_CLERK_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL,
     NEXT_PUBLIC_CLERK_SIGN_UP_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL,
     NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL:
