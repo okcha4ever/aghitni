385a48e2c87282ddb4222deed6fe44b55c0d0a40
okcha4ever <minoukhiari4@gmail.com>
2023-11-04 22:16:56 +0100

    useCheckUser.tsx

--- src/hooks/useCheckUser.ts
+++ src/hooks/useCheckUser.ts
@@ -6,20 +6,20 @@ import axios from "axios";
 // }
 
 const useCheckUser = async (userId: string | null | undefined) => {
-  // try {
-  //   const response = await axios.get(`/api/users/signed-in?userId=${userId}`);
-  //   return response.data;
-  // } catch (err) {
-  //   console.error("Something went wrong in useCheckUser.tsx custom hook", err);
-  //   // Handle the error or decide what to return in case of an error
-  //   // For example, you can return null or throw the error here
-  //   throw err;
-  // }
-  const request = await fetch(
-    `http://localhost:3000/api/users/signed-in?userId=${userId}`,
-  );
-  const data = await request.json();
-  return data;
+  try {
+    const response = await axios.get(`/api/users/signed-in?userId=${userId}`);
+    return response.data;
+  } catch (err) {
+    console.error("Something went wrong in useCheckUser.tsx custom hook", err);
+    // Handle the error or decide what to return in case of an error
+    // For example, you can return null or throw the error here
+    throw err;
+  }
+  // const request = await fetch(
+  //   `http://localhost:3000/api/users/signed-in?userId=${userId}`,
+  // );
+  // const data = await request.json();
+  // return data;
 };
 
 export default useCheckUser;
