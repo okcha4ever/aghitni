2137aabed1cccdc3752ea4a81992163ed6b7032e
med1seghir5 <abdelmalek.medseghir@gmail.com>
2023-11-04 14:45:15 +0400

    sign-in page

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,10 +1,17 @@
+// Sign in page
+import AghitniLogo from "@/components/AghitniLogo";
 import { simple } from "@/helpers/ClerkAppearance";
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <main className="flex items-center justify-center bg-white w-full ">
+    <div id="Sign_in">
+    <main className="flex items-center justify-center sm:flex-row flex-col sm:space-x-20">
+      <AghitniLogo width={120} />
+      <div className="pt-10">
       <SignIn appearance={simple as any} />
+      </div>
     </main>
+    </div>
   );
 }
