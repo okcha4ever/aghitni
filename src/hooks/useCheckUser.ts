09ca3935bb89925b964a09b175ae139a6cd6d610
okcha4ever <minoukhiari4@gmail.com>
2023-11-04 21:58:58 +0100

    works on my machine

--- src/hooks/useCheckUser.ts
+++ src/hooks/useCheckUser.ts
@@ -1,15 +1,25 @@
 import axios from "axios";
 
+// {
+//   userId: string
+//   role: string
+// }
+
 const useCheckUser = async (userId: string | null | undefined) => {
-  try {
-    const response = await axios.get(`/api/users/signed-in?userId=${userId}`);
-    return response.data;
-  } catch (err) {
-    console.error("Something went wrong in useCheckUser.tsx custom hook", err);
-    // Handle the error or decide what to return in case of an error
-    // For example, you can return null or throw the error here
-    throw err;
-  }
+  // try {
+  //   const response = await axios.get(`/api/users/signed-in?userId=${userId}`);
+  //   return response.data;
+  // } catch (err) {
+  //   console.error("Something went wrong in useCheckUser.tsx custom hook", err);
+  //   // Handle the error or decide what to return in case of an error
+  //   // For example, you can return null or throw the error here
+  //   throw err;
+  // }
+  const request = await fetch(
+    `http://localhost:3000/api/users/signed-in?userId=${userId}`,
+  );
+  const data = await request.json();
+  return data;
 };
 
 export default useCheckUser;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,10 @@
 import { authMiddleware } from "@clerk/nextjs";
-
 export default authMiddleware({
   // "/" will be accessible to all users
   // publicRoutes: ["/", "/need-help"],
-  // ignoredRoutes: [/^\/api\/users\/signed-in(.*)/], // Use a regular expression
-  ignoredRoutes: ["/((?!api|trpc))(_next.*|.+.[w]+$)", "/api/users/signed-in"],
+  // ignoredRoutes: ["/api/users/signed-in(.*)"],
 });
 
 export const config = {
-  // matcher: ["/send-help"],
+  matcher: ["/"],
 };
