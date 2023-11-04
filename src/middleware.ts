2febcf15b8729886bf13a3650c619de31a5726d7
okcha4ever <minoukhiari4@gmail.com>
2023-11-04 17:39:14 +0100

    fixed minor bug

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   // "/" will be accessible to all users
   publicRoutes: ["/", "/need-help"],
+  ignoredRoutes: ["/api/users/signed-in(.*)"],
 });
 
 export const config = {
