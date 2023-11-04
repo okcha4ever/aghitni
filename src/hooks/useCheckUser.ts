18669f07221e650f19b623bb9215ad914e745bfb
okcha4ever <minoukhiari4@gmail.com>
2023-11-04 21:07:34 +0100

    fixed 'void' type error on useCheckUser.tsx

--- src/hooks/useCheckUser.ts
+++ src/hooks/useCheckUser.ts
@@ -1,14 +1,15 @@
 import axios from "axios";
 
 const useCheckUser = async (userId: string | null | undefined) => {
-  await axios
-    .get(`/api/users/signed-in?userId=${userId}`)
-    .then((response) => {
-      return response.data;
-    })
-    .catch((err) => {
-      console.log("Something went wrong in useCheckUser.tsx custom hook");
-    });
+  try {
+    const response = await axios.get(`/api/users/signed-in?userId=${userId}`);
+    return response.data;
+  } catch (err) {
+    console.error("Something went wrong in useCheckUser.tsx custom hook", err);
+    // Handle the error or decide what to return in case of an error
+    // For example, you can return null or throw the error here
+    throw err;
+  }
 };
 
 export default useCheckUser;
