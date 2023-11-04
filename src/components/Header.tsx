b9dcb395e3b80c284d6614b7113a42f319264d69
SohaibGhz <ghazusohaib003@gmail.com>
2023-11-04 11:52:38 +0100

    conditional classes in header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,18 @@
-'use client'
+"use client";
 import React from "react";
 import AghitniLogo from "./AghitniLogo";
 import Link from "next/link";
-import {usePathname} from 'next/navigation'
+import { usePathname } from "next/navigation";
 
 const Header = () => {
-  const pathname = usePathname()
+  const pathname = usePathname();
+  const isNeedHelpPage = pathname === "/need-help";
+  const isSendHelpPage = pathname === "/send-help";
+
+  const focusButton = "rounded-full bg-green-700 px-7 py-2 sm:px-5 sm:py-2";
+
+  const inFocusButton = "rounded-full px-7 py-2 hover:bg-green-200 sm:px-5 sm:py-2";
+
   return (
     <center className="w-full">
       <section className="container flex w-full flex-col items-center justify-center gap-5 pt-6">
@@ -14,12 +21,12 @@ const Header = () => {
         </div>
         <div className="flex justify-between rounded-full bg-gray-300 sm:w-80 sm:px-3 sm:py-2  ">
           <Link href="/need-help">
-            <button className="rounded-full bg-green-700 px-7 py-2 sm:px-5 sm:py-2">
+            <button className={isNeedHelpPage ? focusButton : inFocusButton}>
               Need Help
             </button>
           </Link>
           <Link href="/send-help">
-            <button className="rounded-full px-7 py-2 hover:bg-green-200 sm:px-5 sm:py-2">
+            <button className={isSendHelpPage ? focusButton : inFocusButton}>
               Send Help
             </button>
           </Link>
