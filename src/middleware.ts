11b8808bbbc4b06c09a681ef953100a987ae3d06
okcha4ever <minoukhiari4@gmail.com>
2023-11-04 21:31:34 +0100

    ya allah this is for our brothers

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,7 @@ export default authMiddleware({
   // "/" will be accessible to all users
   // publicRoutes: ["/", "/need-help"],
   // ignoredRoutes: [/^\/api\/users\/signed-in(.*)/], // Use a regular expression
+  ignoredRoutes: ["/((?!api|trpc))(_next.*|.+.[w]+$)", "/api/users/signed-in"],
 });
 
 export const config = {
