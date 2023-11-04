344f94c9eb58c8c815af3077bae3f2150e26fabd
okcha4ever <minoukhiari4@gmail.com>
2023-11-04 21:14:27 +0100

    middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
+
 export default authMiddleware({
   // "/" will be accessible to all users
   publicRoutes: ["/", "/need-help"],
-  ignoredRoutes: ["/api/users/signed-in(.*)"],
+  ignoredRoutes: [/^\/api\/users\/signed-in(.*)/], // Use a regular expression
 });
 
 export const config = {
