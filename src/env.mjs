6338b9eb5ece055d047cc6862f0249d82c3491d2
okcha4ever <minoukhiari4@gmail.com>
2023-11-05 18:42:44 +0100

    removed data from .env.example

--- src/env.mjs
+++ src/env.mjs
@@ -17,31 +17,12 @@ export const env = createEnv({
     GOOGLE_CLIENT_SECRET: z.string(),
   },
   client: {
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z
-      .string()
-      .default("pk_test_Y3VyaW91cy1zaGFyay05NC5jbGVyay5hY2NvdW50cy5kZXYk"),
-    NEXT_PUBLIC_CLERK_SECRET_KEY: z
-      .string()
-      .default("sk_test_hfnpirGagJiwlWoaVYr7lfQsDKlRjaW7sQclsaZqFz"),
-    NEXT_PUBLIC_CLERK_SIGN_IN_URL: z.string().default("/sign-in"),
-    NEXT_PUBLIC_CLERK_SIGN_UP_URL: z.string().default("/sign-up"),
-    NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: z.string().default("/"),
-    NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL: z.string().default("/"),
     NEXT_PUBLIC_SECRET: z.string().default("51e350bfc2492baa6d934050b528f197"),
     // Add the new variables
   },
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
-      process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-    NEXT_PUBLIC_CLERK_SECRET_KEY: process.env.NEXT_PUBLIC_CLERK_SECRET_KEY,
-    NEXT_PUBLIC_CLERK_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL,
-    NEXT_PUBLIC_CLERK_SIGN_UP_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL,
-    NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL:
-      process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL,
-    NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL:
-      process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL,
     // Add the new runtime variables
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
